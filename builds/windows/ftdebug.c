f858b8545ff31d16026ae5d9ea2344587654e3f1
Robin Degen <robindegen@gmail.com>
2018-08-16 18:25:45 +0200

    Fixed unresolved symbols on windows due to missing empty trace functions in ftdebug.c.

--- builds/windows/ftdebug.c
+++ builds/windows/ftdebug.c
@@ -233,5 +233,42 @@
 
 #endif /* FT_DEBUG_LEVEL_ERROR */
 
+  FT_BASE_DEF( void )
+  ft_debug_init( void )
+  {
+    /* nothing */
+  }
+
+
+  FT_BASE_DEF( FT_Int )
+  FT_Trace_Get_Count( void )
+  {
+    return 0;
+  }
+
+
+  FT_BASE_DEF( const char * )
+  FT_Trace_Get_Name( FT_Int  idx )
+  {
+    FT_UNUSED( idx );
+
+    return NULL;
+  }
+
+
+  FT_BASE_DEF( void )
+  FT_Trace_Disable( void )
+  {
+    /* nothing */
+  }
+
+
+  /* documentation is in ftdebug.h */
+
+  FT_BASE_DEF( void )
+  FT_Trace_Enable( void )
+  {
+    /* nothing */
+  }
 
 /* END */
